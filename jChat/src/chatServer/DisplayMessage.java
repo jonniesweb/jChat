84ad0900945a5d5a6a7bfe26dd9eba266361c3c4
Jonnie Simpson <jonniesweb@gmail.com>
2011-10-31 01:18:35 -0400

    Adding ContentContainer and DisplayMessage classes

---
+++ jChat/src/chatServer/DisplayMessage.java
@@ -0,0 +1,50 @@
+package chatServer;
+
+import javax.swing.JTextArea;
+
+public class DisplayMessage {
+	
+	private JTextArea tArea;
+	
+	public DisplayMessage(JTextArea textarea) {
+		
+		tArea = textarea;
+	}
+	
+	public void PrintMessage(String message, ContentContainer objmessage) {
+
+		// remove accidental spaces before and after message
+		message.trim();
+
+		message = message + "\n";
+
+		// displays the message to the textarea
+		tArea.append(message);
+		
+		tArea.append(message);
+		
+		if (objmessage != null) {
+			tArea.append(objmessage.getMessageText());
+		}
+
+		// autoscroll the textarea
+		tArea.setCaretPosition(tArea.getDocument().getLength());
+	}
+
+	public void PrintMessage(String message) {
+		tArea.append(message);
+
+	}
+
+	public void PrintMessage(ContentContainer objMessage) {
+
+			String message;
+
+			message = objMessage.getMessageText();
+			message.trim();
+			message += "\n";
+
+
+	}
+
+}
