16e3922862cffd0a54b48fece6353ddaf6e22eae
Jonnie Simpson <jonniesweb@gmail.com>
2011-11-07 00:40:54 -0500

    -Changed default window open position to x100 y100 -Removed password field -Added same action listener as submitbutton to textfield

--- jChat/src/jChat/EnterDialog.java
+++ jChat/src/jChat/EnterDialog.java
@@ -32,7 +32,6 @@ import javax.swing.SwingConstants;
 // extend JDialog so that we can easily call JDialog methods
 public class EnterDialog extends JDialog {
 	private JTextField ipAddressField;
-	private JPasswordField pwdAljgfhsoitbdfogrntgv;
 	private JLabel lblerr = new JLabel("");
 
 	// main method, first thing to run
@@ -52,14 +51,15 @@ public class EnterDialog extends JDialog {
 	}
 
 	public EnterDialog() {
-		
+
 		// set up the contentpane and window
 		getContentPane().setBackground(new Color(77, 77, 77));
 		getContentPane().setForeground(new Color(180, 180, 180));
 		setBackground(new Color(255, 255, 255));
 		setForeground(new Color(0, 0, 0));
+		setBounds(100, 100, 0, 0);
 		setResizable(false);
-		setTitle("jChatClient - Menu");
+		setTitle("jChatClient");
 		setAlwaysOnTop(true);
 		setBounds(100, 100, 402, 205);
 		getContentPane().setLayout(null);
@@ -71,91 +71,53 @@ public class EnterDialog extends JDialog {
 		lblWelcomeToJchatclient.setHorizontalAlignment(SwingConstants.CENTER);
 		lblWelcomeToJchatclient.setAlignmentX(Component.CENTER_ALIGNMENT);
 		lblWelcomeToJchatclient.setSize(new Dimension(392, 0));
-		lblWelcomeToJchatclient.setBounds(new Rectangle(0, 0, 396, 30));
+		lblWelcomeToJchatclient.setBounds(new Rectangle(-1, 0, 396, 30));
 		lblWelcomeToJchatclient.setFont(new Font("Tahoma", Font.BOLD, 14));
 		lblWelcomeToJchatclient.setPreferredSize(new Dimension(392, 30));
 		getContentPane().add(lblWelcomeToJchatclient);
 
-		JLabel lblPleaseEnterThe = new JLabel("Please enter the server IP address and password:");
+		JLabel lblPleaseEnterThe = new JLabel("Enter the server IP address or hostname");
 		lblPleaseEnterThe.setForeground(new Color(180, 180, 180));
 		lblPleaseEnterThe.setBackground(new Color(75, 75, 75));
 		lblPleaseEnterThe.setHorizontalAlignment(SwingConstants.CENTER);
 		lblPleaseEnterThe.setFont(new Font("Tahoma", Font.BOLD, 14));
-		lblPleaseEnterThe.setBounds(0, 30, 396, 30);
+		lblPleaseEnterThe.setBounds(-1, 30, 396, 30);
 		getContentPane().add(lblPleaseEnterThe);
-		
-		// set up the textfield labels
-		JLabel lblIpAddress = new JLabel("IP Address");
-		lblIpAddress.setForeground(new Color(180, 180, 180));
-		lblIpAddress.setBackground(new Color(75, 75, 75));
-		lblIpAddress.setFont(new Font("Tahoma", Font.BOLD, 12));
-		lblIpAddress.setBounds(10, 71, 66, 14);
-		getContentPane().add(lblIpAddress);
 
+		ActionListener actionSubmit = new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+
+				// start the client
+				Client application = new Client(ipAddressField.getText());
+
+				// hide and get rid of the window
+				dispose();
+
+			}
+		};
+		
 		//set up the ipAddressField
 		ipAddressField = new JTextField();
+		ipAddressField.addActionListener(actionSubmit);
 		ipAddressField.setForeground(new Color(180, 180, 180));
 		ipAddressField.setBackground(new Color(65, 65, 65));
-		ipAddressField.setBounds(86, 69, 300, 20);
+		ipAddressField.setBounds(9, 69, 376, 20);
 		getContentPane().add(ipAddressField);
 		ipAddressField.setColumns(10);
-		
-		// set up the password label
-		JLabel lblPassword = new JLabel("Password");
-		lblPassword.setForeground(new Color(180, 180, 180));
-		lblPassword.setBackground(new Color(75, 75, 75));
-		lblPassword.setFont(new Font("Tahoma", Font.BOLD, 12));
-		lblPassword.setBounds(10, 96, 66, 14);
-		getContentPane().add(lblPassword);
-
-		// set up the passwordfield
-		pwdAljgfhsoitbdfogrntgv = new JPasswordField();
-		pwdAljgfhsoitbdfogrntgv.setText("aljgfhso5itbdfogrnt4gv");
-		pwdAljgfhsoitbdfogrntgv.setBackground(new Color(65, 65, 65));
-		pwdAljgfhsoitbdfogrntgv.setForeground(new Color(180, 180, 180));
-		pwdAljgfhsoitbdfogrntgv.setBounds(86, 94, 300, 20);
-		getContentPane().add(pwdAljgfhsoitbdfogrntgv);
 
 		// create a button to process the input
 		JButton btnConnect = new JButton("Connect");
-		
-		// whenever the connect button is pressed
-		btnConnect.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
+		btnConnect.setMaximumSize(new Dimension(200, 25));
+		btnConnect.setAlignmentX(Component.CENTER_ALIGNMENT);
 
-				// fill these variables in from the textfields
-				String ipaddress = ipAddressField.getText();
-				String password = pwdAljgfhsoitbdfogrntgv.getText(); // use getText method even though its depreciated for simplicity
-				
-				// hash the given password
-				int hashpass = password.hashCode();
 
-				// compare the given password hash with the real password hash
-				// you definitely couldn't decompile this then change the if statement
-				if (hashpass == 38272589) {
-					
-					// print to the screen this message
-					lblerr.setText("Password Accepted!");
-					
-					// create and pass the IP address to Client.Client
-					Client application;
 
-					application = new Client(ipaddress);
-					
-					// hide the window
-					setVisible(false);
+		btnConnect.addActionListener(actionSubmit);
 
-				} else {
-					// tell the user the password was wrong
-					lblerr.setText("Wrong Password!");
-
-				} // end else
-
-			}
-		});
-		
 		// set up the button and the error label
-		btnConnect.setBounds(153, 120, 89, 23);
+		btnConnect.setBounds(127, 120, 140, 23);
 		getContentPane().add(btnConnect);
 		lblerr.setForeground(new Color(180, 180, 180));
 		lblerr.setBackground(new Color(75, 75, 75));
