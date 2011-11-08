7fe97dc51be1ac519e48dc4a540fced377d78a3a
Jonnie Simpson <jonniesweb@gmail.com>
2011-11-08 01:07:49 -0500

    -Added proper new line code to prevent all text on one line

--- jChat/src/jChat/DisplayMessage.java
+++ jChat/src/jChat/DisplayMessage.java
@@ -3,26 +3,23 @@ package jChat;
 import javax.swing.JTextArea;
 
 public class DisplayMessage {
-	
+
 	private JTextArea tArea;
-	
+
 	public DisplayMessage(JTextArea textarea) {
-		
+
 		tArea = textarea;
 	}
-	
+
 	public void PrintMessage(String message, ContentContainer objmessage) {
 
 		// remove accidental spaces before and after message
 		message.trim();
-
-		message = message + "\n";
+		message += "\n";
 
 		// displays the message to the textarea
 		tArea.append(message);
-		
-		tArea.append(message);
-		
+
 		if (objmessage != null) {
 			tArea.append(objmessage.getMessageText());
 		}
@@ -32,17 +29,25 @@ public class DisplayMessage {
 	}
 
 	public void PrintMessage(String message) {
+
+		message.trim();
+		message += "\n";
+
+		// displays the message tothe textarea
 		tArea.append(message);
+
+		// autoscroll the textarea
+		tArea.setCaretPosition(tArea.getDocument().getLength());
 
 	}
 
 	public void PrintMessage(ContentContainer objMessage) {
 
-			String message;
+		String message;
 
-			message = objMessage.getMessageText();
-			message.trim();
-			message += "\n";
+		message = objMessage.getMessageText();
+		message.trim();
+		message += "\n";
 
 
 	}
